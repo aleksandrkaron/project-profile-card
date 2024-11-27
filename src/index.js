a82c557eb3a31932160f5ed6087d85fcdae5b42d
Alexandre Caron <alexandrecaron438@pm.me>
2024-11-27 16:01:55 -0500

    Added required components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Avatar() {
+  return <h1>Hello!</h1>;
+}
+
+function Intro() {
+  return <h1>Hello!</h1>;
+}
+
+function SkillList() {
+  return <h1>Hello!</h1>;
+}
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* One skill component for each web dev skill*/}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
